20136b45322449c1b12b42e86decdb90f43749f9
Saul Mijael Choquehuanca Huanca <saulchoque123@gmail.com>
2024-10-29 22:49:57 -0400

    Logo landing page

--- src/app/pages/landing/landing.component.ts
+++ src/app/pages/landing/landing.component.ts
@@ -5,11 +5,13 @@ import { LandingItemComponent } from "./landing-item/landing-item.component";
 import { LANDINGITEMS } from '../../models/constants';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { Router, RouterModule } from '@angular/router';
+import {MatIconModule} from '@angular/material/icon';
+
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [LandingComponent, LandingItemComponent, RouterModule],
+  imports: [LandingComponent, LandingItemComponent, RouterModule, MatIconModule],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.css'
 })
