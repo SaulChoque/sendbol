d497dc52f93089195feedf35a1330e458f2a7a1e
Saul Mijael Choquehuanca Huanca <saulchoque123@gmail.com>
2024-11-04 01:36:55 -0400

    Landing page v1 FINAL

--- src/app/pages/landing/landing.component.ts
+++ src/app/pages/landing/landing.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { landingItem } from '../../models/landingItem';
-import { MediaQueryService } from '../../services/media-query.service';
 import { LandingItemComponent } from "./landing-item/landing-item.component";
 import { LANDINGITEMS } from '../../models/constants';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { Router, RouterModule } from '@angular/router';
 import {MatIconModule} from '@angular/material/icon';
 
+import { MediaQueryService } from '../../services/media-query.service';
+import { Breakpoints } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-landing',
@@ -18,12 +19,27 @@ import {MatIconModule} from '@angular/material/icon';
 export class LandingComponent {
 
   landingintems = [...LANDINGITEMS];
+  constructor(
+    private mediaQueryService: MediaQueryService,
+  ) {}
+
+  @Input()
+  breakpoints = Breakpoints;
+  currentBreakpoint?: string;
+  currentWidth?: string;
 
 
+  ngOnInit() {
 
-  #mediaQueryService: MediaQueryService;
+    this.mediaQueryService.breakpoint$.subscribe(() => {
+      this.mediaQueryService.triggerProcesses();
+      this.currentBreakpoint = this.mediaQueryService.returnBreakpoint();
+    });
 
-  constructor(mediaQueryService: MediaQueryService) {
-    this.#mediaQueryService = mediaQueryService;
+
+    this.currentWidth = this.mediaQueryService.returnWidth();
+
+
+    //console.log(this.constantes.IMGBAN001);
   }
 }
